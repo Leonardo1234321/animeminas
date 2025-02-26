861eb2db094976e90c062e66fd78701986e9a74b
Jurandy Soares <jurandy.soares@escolar.ifrn.edu.br>
2025-02-26 13:36:33 +0000

    feat: Função que representa a fala de um personagem

--- pacote/nucleo.py
+++ pacote/nucleo.py
@@ -3,6 +3,12 @@ import os
 import turtle
 from pathlib import Path
 
+# Configurações do balão
+global balao
+balao = turtle.Turtle()
+balao.hideturtle()
+balao.down()
+
 def carrega_personagens():
     cam_personagens = Path(__file__).parent / "personagens"
     cam_atual = Path.cwd()
@@ -12,4 +18,20 @@ def carrega_personagens():
         turtle.register_shape(img)
 
     os.chdir(cam_atual.as_posix())
+
+def fala(personagem: turtle.Turtle, 
+         texto: str, 
+         angulo: int, 
+         distancia: int,
+         tempo: float):
+    
+    x,y = personagem.pos()
+    balao.goto(x, y)   
+    balao.left(angulo)
+    balao.forward(distancia)
+    balao.write(fala)
+    sleep(tempo)
+    balao.undo()
+    balao.undo()
+    balao.undo()
 
